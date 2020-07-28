5dffbce81263dab40f47333ea1206250801190f9
hXR16F <hxr16f.ar@gmail.com>
2020-07-28 17:08:57 +0200

    Add files via upload

--- discord-spy.bat
+++ discord-spy.bat
@@ -1,26 +1,32 @@
+:: Programmed by hXR16F
+:: hXR16F.ar@gmail.com, https://github.com/hXR16F
+
 @echo off
 setlocal EnableDelayedExpansion
+set "name=discord-spy"
 
-for /f "tokens=2 delims=@" %%i in (
-	'tasklist /v ^| findstr /i /c:"discord"'
-) do (
-	for /f "tokens=1 delims= " %%j in ("%%i") do (
+echo [%name%] Fetching data...
+pushD "%temp%"
+tasklist /v | findstr /i /c:"discord" > "%name%.txt"
+
+for /f "tokens=2 delims=@" %%i in (%name%.txt) do (
+	for /f "tokens=1 delims=-" %%j in ("%%i") do (
 		set "person=%%j"
-		echo [discord-spy] Person you've messaged recently: !person!.
+		echo [%name%] Person you've messaged recently: !person:~0,-1!.
 	)
 )
 
 if not defined person (
-	for /f "tokens=2 delims=#" %%i in (
-		'tasklist /v ^| findstr /i /c:"discord"'
-	) do (
+	for /f "tokens=2 delims=#" %%i in (%name%.txt) do (
 		for /f "tokens=1 delims= " %%j in ("%%i") do (
 			set "channel=%%j"
-			echo [discord-spy] Channel you've visited recently: !channel!.
+			echo [%name%] Channel you've visited recently: !channel!.
 		)
 	)
 )
 
-if not defined person if not defined channel echo [discord-spy] Cannot find person/channel.
+if not defined person if not defined channel echo [%name%] Cannot find person/channel.
+if exist "%name%.txt" del /f /q "%name%.txt" >nul
+popD
 
-pause
+goto :eof
